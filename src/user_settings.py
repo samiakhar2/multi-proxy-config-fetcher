b1ab204c47955bb3db173bfe97454b78430d9aa2
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-10-24 11:42:44 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -59,8 +59,8 @@ SINGBOX_TESTER_TIMEOUT_SECONDS = 10
 # List of URLs to test sing-box configs against.
 # The tester will try each URL in order until one succeeds.
 SINGBOX_TESTER_URLS = [
-    'https://www.youtube.com/generate_204',
-    'https://www.gstatic.com/generate_204'
+    'https://www.youtube.com/generate_204'
+    #'https://www.gstatic.com/generate_204'
 ]
 
 # --- Xray Config Tester Settings ---
@@ -81,6 +81,6 @@ XRAY_TESTER_TIMEOUT_SECONDS = 10
 # List of URLs to test Xray configs against.
 # The tester will try each URL in order until one succeeds.
 XRAY_TESTER_URLS = [
-    'https://www.youtube.com/generate_204',
-    'https://www.gstatic.com/generate_204'
+    'https://www.youtube.com/generate_204'
+    #'https://www.gstatic.com/generate_204'
 ]
