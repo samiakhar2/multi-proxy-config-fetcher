89eb8e19115ccbf9e92c2e8ea7c671b4649c1d0a
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-10-25 20:25:25 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -9,12 +9,12 @@ SOURCE_URLS = [
     "https://t.me/s/prrofile_purple",
     "https://t.me/s/DirectVPN",
     "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
-    "https://raw.githubusercontent.com/imegabiz/ss-config-updater/refs/heads/main/configs.txt",
-    "https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
-    "https://raw.githubusercontent.com/yebekhe/vpn-fail/refs/heads/main/sub-link.txt",
-    "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html",
     "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt",
+    "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html",
     "https://raw.githubusercontent.com/darkvpnapp/CloudflarePlus/refs/heads/main/proxy",
+    "https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
+    #"https://raw.githubusercontent.com/imegabiz/ss-config-updater/refs/heads/main/configs.txt",
+    #"https://raw.githubusercontent.com/yebekhe/vpn-fail/refs/heads/main/sub-link.txt",
 ]
 
 # Set to True to fetch the maximum possible number of configurations.
